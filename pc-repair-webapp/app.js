5ed3398cba3c376aac0ef477a16e312325638dfd
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-08-11 15:16:12 +0800

    Added About Page

--- pc-repair-webapp/app.js
+++ pc-repair-webapp/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var index = require('./routes/index');
+var about = require('./routes/about');
+
 var app = express();
 
 // view engine setup
@@ -19,8 +22,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
