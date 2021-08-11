48becafecb2b04f0bd331e71966c8b66239c10ad
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-08-11 15:54:59 +0800

    Added Services Page

--- pc-repair-webapp/app.js
+++ pc-repair-webapp/app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 
 var index = require('./routes/index');
 var about = require('./routes/about');
+var services = require('./routes/services');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/about', about);
+app.use('/services', services);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
